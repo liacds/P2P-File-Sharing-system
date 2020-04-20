9e9789a51a1e67cf0bd9116bf09e52a85bc1b4e7
ZhanShy <39809064+ZhanShy@users.noreply.github.com>
2020-04-20 11:44:27 +0600

    Add files via upload

---
+++ server.py
@@ -0,0 +1,65 @@
+# made by Aliya Almas and Akzhan Suranshy
+from socket import *
+serverPort = 9999
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('',serverPort))
+serverSocket.listen(1)
+print ('The server is ready to receive:')
+
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    message = connectionSocket.recv(1024)
+    
+    #    print (message,'::',message.split()[0],':',message.split()[1])
+    filename = message.split()[1]
+    print (filename,'||',filename[1:])
+    encoding = 'utf-8'
+    lastchar3=filename[-3:].decode('ASCII')
+    lastchar4=filename[-4:].decode('ASCII')
+
+    if (lastchar3=="jpg" or lastchar4=="jpeg"):
+        
+        try:
+            f=open(filename[1:], "r+b")
+            data = f.read()
+            f.close()
+            HTTP_RESPONSE = b'\r\n'.join([
+                              b"HTTP/1.1 200 OK",
+                              b"Connection: close",
+                              b"Content-Type: image/jpg",
+                              bytes("Content-Length: %s" % len(data),'utf-8'),
+                              b'', data
+                              ] )
+            print(HTTP_RESPONSE)
+            connectionSocket.sendall(HTTP_RESPONSE)
+            break
+
+
+        
+        except IOError:
+            print("404 Not Found")
+            connectionSocket.send("HTTP/1.1 404 Not Found  \r\n\r\n 404 Not Found".encode())
+            break
+    else:
+        try:
+            f = open(filename[1:])
+            outputdata = f.read()
+            f.close()
+            print (outputdata)
+            HTTP_RESPONSE = b'\r\n'.join([
+                                          b"HTTP/1.1 200 OK",
+                                          b"Connection: close",
+                                          b"Content-Type: text/txt/html",
+                                          bytes("Content-Length: %s" % len(outputdata),'utf-8'),
+                                          b'', str.encode(outputdata)
+                                          ] )
+            print(HTTP_RESPONSE)
+            connectionSocket.send("HTTP/1.1 200 OK  \r\n\r\n 200 OK\n".encode())
+            connectionSocket.send(str.encode(outputdata))
+            connectionSocket.close()
+            break
+
+        except IOError:
+            print("404 Not Found")
+            connectionSocket.send("HTTP/1.1 404 Not Found  \r\n\r\n 404 Not Found".encode())
+            break
