956b0189e7a1d0b2d15a00ab5fc1c4b40facff92
Aliya Almas <itisaliya@gmail.com>
2020-04-21 02:46:22 +0600

    ordering

--- client.py
+++ client.py
@@ -14,14 +14,14 @@ def open_gui(clientSocket):
 
     root = Tk()
 
-    SearchBox(root, placeholder="Type and press enter", entry_highlightthickness=0).pack(pady=10, padx=10)
+    #SearchBox(root, placeholder="Type and press enter", entry_highlightthickness=0).pack(pady=10, padx=10)
     root.geometry("600x600")
     root.title("File sharing")
     upload = UploadFile(root, clientSocket, serverPort)
     upload.pack(pady=10, padx=10)
 
     
-    Results(root).pack(pady=10, padx=10)
+    #Results(root).pack(pady=10, padx=10)
     #file_info = upload.browse()
     # вот здесь короче будет 
     #  message = "<" + file_info + ">"
@@ -39,7 +39,7 @@ def open_gui(clientSocket):
 
 
 serverName='localhost'
-serverPort = 9998
+serverPort = 9999
 # ip=199.11.11.11
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
